c01893513711917e4bb32f59afa297764378171b
Hyan Mandian <hyanmandian@hotmail.com>
2018-05-08 01:16:11 -0300

    :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Hyan Mandian',
     author: 'Hyan Mandian',
-    description: 'A simple tool to help me share things I know.',
+    description: 'A simple tool to help me share things I learned.',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
